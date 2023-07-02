330121a693da0a889bda23bb288f2d1ef7ee0f7d
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2023-07-02 20:56:38 -0300

    First Commit

---
+++ src/test/java/br/com/rafael/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
